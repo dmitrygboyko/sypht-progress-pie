0c4d10cbc52c90fcbd5784aab7b6df6a20b949a5
Dmitry <dmitrygboyko@gmail.com>
2020-01-29 17:25:53 +0200

    Update Program.cs for any number of cases

--- sypht-progress-pie/Program.cs
+++ sypht-progress-pie/Program.cs
@@ -6,10 +6,36 @@ namespace sypht_progress_pie
 	{
 		static void Main(string[] args)
 		{
-			var pointColorCalculator = new PointColorCalculator();
-			var result = pointColorCalculator.Calculate(13, 55, 55);
-			Console.WriteLine(result);
+			try
+			{
+				Console.Write("Please enter number of coordinates: ");
+				var numberOfCoordinates = int.Parse(Console.ReadLine());
+
+				for (var i = 0; i < numberOfCoordinates; i++)
+				{
+					Console.Write("Please enter percentage and coordinates: ");
+					var coordinatesString = Console.ReadLine();
+					var (percentage, x, y) = ParseParams(coordinatesString);
+					var pointColorCalculator = new PointColorCalculator();
+					var result = pointColorCalculator.Calculate(percentage, x, y);
+					Console.WriteLine(result);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
 			Console.ReadLine();
+		}
+
+		private static (int Percentage, int X, int Y) ParseParams(string coordinates)
+		{
+			var coordinateParts = coordinates.Trim().Split(' ');
+			var percentage = int.Parse(coordinateParts[0].Trim());
+			var x = int.Parse(coordinateParts[1].Trim());
+			var y = int.Parse(coordinateParts[2].Trim());
+
+			return (percentage, x, y);
 		}
 	}
 }
